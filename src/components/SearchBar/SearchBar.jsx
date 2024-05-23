b9d2b7f1d5681944c1e3cb864ce5c97940ae71cf
EstelleLaucher <estellelaucher.pro@gmail.com>
2024-05-23 14:28:45 +0200

    Css Searchbar with background image mobile version

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,22 +2,23 @@ import './SearchBar.scss';
 
 import { Input, Form, FormField, Segment } from 'semantic-ui-react';
 
-//import image from '../../assets/image/background-img.jpg';
-
 const SearchBar = () => {
   return (
-    <div className="SearchBar">
-      <Segment>
-        <Form>
-          <FormField>
-            <Input
-              icon="search"
-              iconPosition="right"
-              placeholder="Rechercher un jeu"
-            />
-          </FormField>
-        </Form>
-      </Segment>
+    <div className="SectionSearch">
+      <h1>Louez-les tous !</h1>
+      <div className="SearchBar">
+        <Segment>
+          <Form>
+            <FormField>
+              <Input
+                icon="search"
+                iconPosition="right"
+                placeholder="Rechercher un jeu"
+              />
+            </FormField>
+          </Form>
+        </Segment>
+      </div>
     </div>
   );
 };
