88df56b6d4237d2166ccd8f5360d6d22a4ca428a
EstelleLaucher <estellelaucher.pro@gmail.com>
2024-05-24 09:36:13 +0200

    chnage color home page

--- src/components/NavigationMobile/NavigationMobile.jsx
+++ src/components/NavigationMobile/NavigationMobile.jsx
@@ -3,26 +3,26 @@ import './NavigationMobile.scss';
 const NavigationMobile = () => {
   return (
     <div className="NavigationMobile">
-      <div className="BlocIcon">
+      <a href="*" className="BlocIcon">
         <img src="/src/assets/icon/pawn-icon2.png" alt="pion" />
         <span>Les jeux</span>
-      </div>
-      <div className="BlocIcon">
+      </a>
+      <a href="*" className="BlocIcon">
         <img src="/src/assets/icon/fav2.png" alt="favoris" />
         <span>Mes favoris</span>
-      </div>
-      <div className="BlocIcon">
+      </a>
+      <a href="*" className="BlocIcon">
         <img src="/src/assets/icon/loupe-icone2.png" alt="loupe recherche" />
         <span>Recherches</span>
-      </div>
-      <div className="BlocIcon">
+      </a>
+      <a href="*" className="BlocIcon">
         <img src="/src/assets/icon/agenda-icon2.png" alt="agenda" />
         <span>Mes réservation</span>
-      </div>
-      <div className="BlocIcon">
+      </a>
+      <a href="*" className="BlocIcon">
         <img src="/src/assets/icon/avatar2.png" alt="compte" />
         <span>connexion</span>
-      </div>
+      </a>
     </div>
   );
 };
