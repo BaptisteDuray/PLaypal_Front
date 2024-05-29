73d1832f05e255517497febb1ea1eeef6072559f
EstelleLaucher <estellelaucher.pro@gmail.com>
2024-05-29 16:35:48 +0200

    authmiddleware back front

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,33 +1,60 @@
-import { SUBMIT_LOGIN } from '../actions/search';
+import axios from 'axios';
 
+import {
+  SUBMIT_LOGIN,
+  HANDLE_SUCCESSFUL_LOGIN,
+  handleSuccessLogin,
+} from '../actions/search';
+//HANDLE_SUCCESSFUL_LOGIN
 // A middleware for the authentification (here to manage the communication with the server for the authentification)
 
 const authMiddleware = (store) => (next) => (action) => {
-  /* switch (action.type) {
+  switch (action.type) {
     case SUBMIT_LOGIN:
       axios
         .post(
           // URL
-          'http://baptisteduray-server.eddi.cloud/login',
-          // données http://baptisteduray-server.eddi.cloud/login
+          'https://backend.baptisteduray-server.eddi.cloud/api/login_check',
+          // données https://backend.baptisteduray-server.eddi.cloud/
           {
-            email: store.getState().email,
+            username: store.getState().email, //remplacer username par email si ça ne marche pas
             password: store.getState().password,
             // Note : si on veut accéder à une information de payload de l'action,
             // => truc: action.nomInfo
           }
         )
         .then((response) => {
+          console.log(response);
           //il faut que je change response.data.pseudo selon comment on l'a appelé
-          const actionToDispatch = handleSuccessFulLogin(response.data.pseudo);
-          store.dispatch(actionToDispatch);
+          //const actionToDispatch = handleSuccessFulLogin(
+          // response.user.firstname
+          //);
+          //console.log('Response from server:', response.data);
+          //const actionToDispatch = {
+          //type: HANDLE_SUCCESSFUL_LOGIN,
+          //payload: response.data,
+          // };
+
+          //store.dispatch(
+          //handleSuccessfulLogin(
+          //  response.playpal.game.firstname,
+          // response.playpal.token
+          // )
+          //);
+
+          // à ce stade le token est déjà rangé dans le state (le store traite
+          // les actions de façon synchrone)
+
+          // on récupère les recettes préférées de l'utilisateur
+          //store.dispatch(handleSuccessLogin());
+          //store.dispatch(actionToDispatch);
         })
         .catch((error) => {
           console.log(error);
         });
       break;
     default:
-  }*/
+  }
 
   next(action);
 };
--- src/middleware/gameListMiddleware.js
+++ src/middleware/gameListMiddleware.js
@@ -1,7 +1,25 @@
 import axios from 'axios';
 
-const gameListMiddleware = (store) => (next) => (action) => {
-  next(action);
+import { FETCH_GAMES, saveGames } from '../actions/search';
+
+const gamesListMiddleware = (store) => (next) => (action) => {
+  switch (action.type) {
+    case FETCH_GAMES:
+      axios
+        .get('https://backend.baptisteduray-server.eddi.cloud/api/jeu')
+        .then((response) => {
+          //console.log(response);
+          store.dispatch(saveGames(response.data));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      break;
+
+    default:
+      break;
+  }
 };
 
-export default gameListMiddleware;
+export default gamesListMiddleware;
